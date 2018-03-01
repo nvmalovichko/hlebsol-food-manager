4aba3ddd27673f0fe040b20397da94d4e70531b8
Nikita Malovichko <nikita.malovichko@mercaux.com>
2018-03-01 17:15:21 +0300

    allowed hosts fix

--- hlebsol/settings.py
+++ hlebsol/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'onzvrp&+_08k!j*r%0ykcvakoie3@@ygellzod-mv_8z5ydd3e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sleepy-sea-38666.herokuapp.com', '127.0.0.1']
 
 LOGIN_REDIRECT_URL = 'menu'
 
