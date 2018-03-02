30933f18b2f9b185240ce3b60c7746afc10de9ec
Nikita Malovichko <nikita.malovichko@mercaux.com>
2018-03-02 17:49:23 +0300

    static and media

--- hlebsol/settings.py
+++ hlebsol/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 try:
