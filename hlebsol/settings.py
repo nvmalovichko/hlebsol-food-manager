455feb359c879d4d2d907cb0c41ae8552ee520e7
Nikita Malovichko <nikita.malovichko@mercaux.com>
2018-03-02 18:03:40 +0300

    whitenoise static

--- hlebsol/settings.py
+++ hlebsol/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +128,8 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from .local import *
