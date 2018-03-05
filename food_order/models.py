4b6015d1690ab59a19ce55aeea2575434c323b02
Nikita Malovichko <nikita.malovichko@mercaux.com>
2018-03-05 13:37:53 +0300

    group by username

--- food_order/models.py
+++ food_order/models.py
@@ -68,7 +68,7 @@ class FoodOffer(models.Model):
             recent_orders = recent_orders.filter(user=user)
         recent_orders = recent_orders.select_related('menu_item')
         grouped_orders_by_user = [(user_name, list(food_orders)) for user_name, food_orders
-                                  in itertools.groupby(recent_orders, lambda x: x.user.get_full_name())]
+                                  in itertools.groupby(recent_orders, lambda x: x.user.username)]
         grouped_orders_by_user_extended = [(user_name, grouping_orders(food_orders), calculate_price(food_orders))
                                            for user_name, food_orders in grouped_orders_by_user]
         return sorted(grouped_orders_by_user_extended, key=lambda x: x[0])
